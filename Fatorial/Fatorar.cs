87a9f5d0dc87fb4d99d87a34ac3c4ae1b152f329
202120423 <202120423@TPCP04LAB1010>
2023-08-23 20:49:46 -0300

    Adicionar arquivos de projeto.

---
+++ Fatorial/Fatorar.cs
@@ -0,0 +1,24 @@
+﻿using Fatorial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fatorial
+{
+    public class Fatorar
+    {
+        public static int Calcular(int num)
+        {
+            if ((num == 0) || (num == 1))
+            {
+                return 1;
+            }
+            else
+            {
+                return num * Calcular(num - 1);
+            }
+        }
+    }
+}
---
+++ Fatorial/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Fatorial;
+
+
+//variaveis. "Num" armazena.
+Console.WriteLine("Digite um número para calcular o fatorial: ");
+int num = int.Parse(Console.ReadLine());
+
+int resultado = Fatorar.Calcular(num);
+Console.WriteLine($"O Fatorial de {num} é {resultado}.");
